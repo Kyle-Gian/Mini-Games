98751d9905aebf261ab050673166dd8c257944a2
Kyle Gian <kyle.gian@hotmail.com>
2020-12-06 17:21:41 +1100

    Added some materials for game objects, Added game timer script and finish line script

---
+++ Mini_Games/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+
+    [HideInInspector]
+    public static float _finalTime;
+
+    [SerializeField] TextMeshProUGUI timerText;
+    public static bool timerFinished = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += 1 * Time.deltaTime;
+        DisplayTime(currentTime);
+        _finalTime = currentTime;
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); //converts time to minutes
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60); // converts time 
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
